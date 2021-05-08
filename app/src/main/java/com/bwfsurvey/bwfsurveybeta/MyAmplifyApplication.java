1019e4ef81baebcf166de2e89afbf04c39123ac7
snahammond <niipad4g@gmail.com>
2021-05-08 11:29:38 +0200

    new token and config for main application

--- app/src/main/java/com/bwfsurvey/bwfsurveybeta/MyAmplifyApplication.java
+++ app/src/main/java/com/bwfsurvey/bwfsurveybeta/MyAmplifyApplication.java
@@ -220,7 +220,7 @@ public class MyAmplifyApplication extends Application {
         ArrayList<Config> configs = null;
         //load from file
         ConfigXmlParser configXmlParser = new ConfigXmlParser();
-        InputStream is = getResources().openRawResource(R.raw.config_old);
+        InputStream is = getResources().openRawResource(R.raw.config);
 
         try {
             configs = (ArrayList<Config>) configXmlParser.parse(is);
